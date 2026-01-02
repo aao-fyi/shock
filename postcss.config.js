24706ba90af90dc8cad58c94d5da4eb529580acf
aao.fyi <dev@aao.fyi>
2026-01-01 20:28:41 -0500

    Remove unused styles

--- postcss.config.js
+++ postcss.config.js
@@ -12,31 +12,21 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   sourceMap: false,
   safelist: {
     standard: [
-      'active',
-      'badge',
-      'bg-primary', 'bg-secondary',
       'bottom-0',
-      'btn-link', 'btn-primary', 'btn-outline-primary',
-      'collapse', 'collapsing',
-      'col-xxl-10',
       'd-block', 'd-flex',
       'end-0',
       'fade',
       'flex-column', 'fs-lg', 'fs-sm', 'fs-xl', 'fs-xs', 'fst-italic',
-      'justify-content-center',
-      'm-1', 'm-2', 'mb-0', 'mb-1', 'my-1', 'my-2', 'mx-2', 'mb-2',
+      'hide', 'hiding',
       'list-unstyled',
       'offcanvas-backdrop',
       'opacity-0', 'opacity-50', 'opacity-100', 'overflow-hidden',
-      'p-1', 'pb-0', 'pe-3', 'pt-0', 'pt-1', 'px-2', 'py-1', 'px-3', 'py-2', 'p-2',
       'position-absolute', 'position-fixed', 'position-relative', 'position-sticky',
       'rounded', 'rounded-top',
       'show', 'showing',
       'start-50',
       'text-nowrap', 'text-pre-wrap', 'text-uppercase', 'text-white', 'top-0', 'top-50',
-      'text-bg-secondary', 'text-success', 'text-truncate',
       'user-select-all',
-      'w-100',
       'was-validated'
     ],
     deep: [
@@ -46,7 +36,6 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
     ]
   },
   dynamicAttributes: [
-    'data-bs-popper',
     'data-bs-target',
     'data-bs-theme',
     'data-lang',
