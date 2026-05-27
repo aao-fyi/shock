1637851a762c7dc9cbad0b85682d17a8e4ff1341
aao.fyi <dev@aao.fyi>
2026-05-27 11:33:38 -0400

    Update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -12,34 +12,23 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   sourceMap: false,
   safelist: {
     standard: [
+      'highlight', 'highlight-copy', 'highlight-wrapper',
       'bottom-0',
-      'd-block', 'd-flex',
+      'd-block', 'd-flex', 'd-inline',
       'end-0',
       'fade',
-      'flex-column', 'fs-lg', 'fs-sm', 'fs-xl', 'fs-xs', 'fst-italic',
       'hide', 'hiding',
-      'list-unstyled',
-      'offcanvas-backdrop',
       'opacity-0', 'opacity-50', 'opacity-100', 'overflow-hidden',
       'position-absolute', 'position-fixed', 'position-relative', 'position-sticky',
       'rounded', 'rounded-top',
       'show', 'showing',
       'start-50',
-      'text-nowrap', 'text-pre-wrap', 'text-uppercase', 'text-white', 'top-0', 'top-50',
-      'user-select-all',
-      'was-validated'
+      'top-0', 'top-50'
     ],
-    deep: [
-      /^highlight$/,
-      /^highlight-copy$/,
-      /^highlight-wrapper$/
-    ]
+    deep: []
   },
   dynamicAttributes: [
-    'data-bs-target',
     'data-bs-theme',
-    'data-lang',
-    'data-palette',
     'role'
   ]
 });
